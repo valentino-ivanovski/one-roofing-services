860c489f31f138de9304b074a04dad3c99765026
Valentino Ivanovski <valentino.ivanovski@icloud.com>
2025-05-15 01:38:17 +0200

    insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
