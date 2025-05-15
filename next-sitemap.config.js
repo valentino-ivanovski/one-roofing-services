a0dae4c3c7c77442c3f93f31502ea575ca6b7ac0
Valentino Ivanovski <valentino.ivanovski@icloud.com>
2025-05-15 02:06:18 +0200

    sitemap and robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://one-roofing.vercel.app', // 🔁 Replace with your actual domain
+  generateRobotsTxt: true, // ✅ Also generates robots.txt
+  changefreq: 'weekly',
+  priority: 0.7,
+}
